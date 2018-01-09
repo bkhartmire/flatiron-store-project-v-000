49c39102ac008d94ed68c46c5c4573646ff35fb3
bkhartmire <bkhartmire@gmail.com>
2018-01-08 22:41:48 -0800

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,9 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
 
+  def add_item(item)
+    self.line_items.create(cart_id: self.id, item_id: item.id)
+  end
 end
