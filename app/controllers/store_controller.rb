c971161100606aace57eda8e5d687cf5cd5b5beb
bkhartmire <bkhartmire@gmail.com>
2018-01-10 02:06:17 -0800

    Done.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @categories = Category.all
-    @items = Item.all.collect{|item| item.inventory > 0}
+    @items = Item.all.select{|item| item.inventory > 0}
   end
 end
